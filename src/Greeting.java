83563c9c70304e00f331fa47f6fdbe4d9e5d6853
Ajith Kumar <ajith_kotian@yahoo.com>
2015-02-28 14:57:58 +0800

    First file

--- src/Greeting.java
+++ src/Greeting.java
@@ -4,5 +4,7 @@ public class Greeting {
 	public static void main(String[] args) {
 		System.out.println("Good Day");
 		System.out.println("Good Afternoon");
+		System.out.println("Good Morning");
+		System.out.println("Good Evening");
 	}
 }
